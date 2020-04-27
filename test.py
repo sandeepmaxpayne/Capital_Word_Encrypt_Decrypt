a3444ef334a21c42bf65cd177e0efd1cf027ede7
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2020-04-27 17:31:55 +0530

    testing with minimum word length 3 successful but not less than 3 and not for infinte length word

--- test.py
+++ test.py
@@ -1,9 +1,25 @@
 import encryptText
 import decryptText
 
-def test():
-    encrypted_texts, secret_keys, text_order = encryptText.encryptStr("SAnd124Panye")
-    print(f'encrypted_texts: {encrypted_texts}, secretskeys: {secret_keys}, encrypted_text_order: {text_order}')
+''' Each words makes a sentence'''
 
-    print(decryptText.decryptText(encrypted_texts, secret_keys, text_order))
+def test():
+    sentence = "HELLO sandeep what isin SAMCABcoffee124"
+    sentence = sentence.split()
+    encrypted_sentence, keylist, order,  = [], [], []
+
+    for i in sentence:
+        encrypted_texts, secret_keys, text_order = encryptText.encryptStr(i)
+        # print(f'encrypted_texts: {encrypted_texts}, secretskeys: {secret_keys}, encrypted_text_order: {text_order}')
+        encrypted_sentence.append(encrypted_texts)
+        keylist.append(secret_keys)
+        order.append(text_order)
+    print(encrypted_sentence, keylist, order)
+    print(f'encrypted sentence: {" ".join(encrypted_sentence)}')
+
+    '''For decrypting each word '''
+    decrypted_result = []
+    for i in range(len(sentence)):
+        decrypted_result.append(decryptText.decryptText(encrypted_sentence[i], keylist[i], order[i]))
+    print(" ".join(decrypted_result))
 test()
